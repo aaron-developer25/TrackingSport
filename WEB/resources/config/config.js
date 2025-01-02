37ef9df87292dbeedfa192df5a514e11538fdaec
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2025-01-02 18:32:57 -0400

    Se agrega el archivo de configuracion

---
+++ WEB/resources/config/config.js
@@ -0,0 +1,11 @@
+const urlWeb = document.location.origin;
+
+const controllers = {
+    controllersAlmacenamiento: urlWeb + '/resources/controllers/AlmacenamientoControllers.php'
+};
+
+const config = {
+    ajusteId: 1,
+    urlDesarrollador: "https://aarondeveloper.000.pe",
+    empresaDesarrollador: "INSERVIS COMPUTER"
+};
